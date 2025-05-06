47f1fbbec46842168c2591d1ee4c2b06518fd54d
zain <rbuma8426@gmail.com>
2025-05-06 22:05:24 +0700

    chore: more harpoon file

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -21,6 +21,9 @@ return {
     vim.keymap.set('n', '<leader>4', function()
       ui.nav_file(4)
     end, { desc = 'Harpoon: Navigate to file 4' })
+    vim.keymap.set('n', '<leader>5', function()
+      ui.nav_file(5)
+    end, { desc = 'Harpoon: Navigate to file 5' })
 
     -- Optional: Toggle previous & next buffers
     vim.keymap.set('n', '<C-S-P>', function()
